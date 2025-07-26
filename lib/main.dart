ae0d253d244bfc8ece561e83253cf7f84e5eec4d
upendrashrestha10 <shresthaupendra1234@gmail.com>
2025-07-26 10:38:15 +0545

    firebase setup in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:wallpaper_app/Admin/admin_login.dart';
+import 'package:wallpaper_app/pages/bottomnav.dart';
 
 
 
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -18,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.black,
       ),
-      home:AdminLogin(),
+      home:BottomNav(),
     );
   }
 }
